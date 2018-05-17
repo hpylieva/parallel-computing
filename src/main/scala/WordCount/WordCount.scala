f7752e0c6cce60d1f9a7bf83ef4696f8b54530e1
Hanna Pylieva <pylyeva@gmail.com>
2018-05-18 00:22:22 +0300

    WordCount WIP

--- src/main/scala/WordCount/WordCount.scala
+++ src/main/scala/WordCount/WordCount.scala
@@ -7,44 +7,56 @@ object WordCount{
     def zero: A
   }
 
+  object WordCounter{
+//     I need this method to be static (belong to a class not an object of a class)
+    def initWordCounter(text: String): WordCounter = {
+      val words = text.split("\\W+").toList
+//      val firstPart = text.takeWhile(_ != ' ')
+//      val lastPart = text.split(" ").last
+//      val middleWordCount = text.split(" ").length - 2
+//      println(firstPart,middleWordCount, lastPart)
+//      new WordCounter(firstPart, middleWordCount, lastPart)
+      println(words.head, words.length - 2, words.last)
+      new WordCounter(words.head, words.length - 2, words.last)
+    }
+  }
+
   case class WordCounter(leftPart: String, countWords: Int, rightPart: String)
 
   val wordCountMonoid =  new Monoid[WordCounter] {
     override def op(x: WordCounter, y: WordCounter): WordCounter =
-      WordCounter(x.leftPart, x.countWords+y.countWords +1, y.rightPart)
+      WordCounter(x.leftPart, x.countWords + y.countWords + 1, y.rightPart)
     override def zero: WordCounter = WordCounter("",0,"")
   }
 
-  def initWordCounter(text: String): WordCounter = {
-    val firstPart = text.takeWhile(_ != ' ')
-    val lastPart = text.split(" ").last
-    val middleWordCount = text.split(" ").length - 2
-    new WordCounter(firstPart, middleWordCount, lastPart)
-  }
 
-  def splitLineAndCountWords(text: String)(implicit threshold:Int) : Int = {
-
+  def splitLineAndCountWords(text: String)(implicit maxSignsThreshold:Int) : Int = {
     def iter(text: String): WordCounter = {
-      if (text.length < threshold)
-        initWordCounter(text)
+      if (text.length < maxSignsThreshold)
+        WordCounter.initWordCounter(text)
       else {
         val (left, right) = text.splitAt(text.length / 2)
-        wordCountMonoid.op(new WordCounter(left, 0, right))
-
+        wordCountMonoid.op(WordCounter.initWordCounter(left),WordCounter.initWordCounter(right))
       }
     }
+
+    val result = iter(text)
+    // add 2 as we have words in left and right parts
+    result.countWords + 2
   }
 
   def getLinesFromFile(filename: String) = {
-    val lines = Source.fromFile(filename).getLines.mkString
-    println(s"The file $filename contains ${lines.length} lines.")
+    val lines = Source.fromFile(filename).getLines.mkString("\n")
+//    println(s"The file $filename contains ${lines.length} lines.")
     lines
   }
 
-  implicit val threshold: Int = 1
+
 
   def main(args: Array[String]): Unit = {
-    print(getLinesFromFile("src/main/scala/WordCount/small.txt"))
+    val text = getLinesFromFile("src/main/scala/WordCount/small.txt")
+    implicit val threshold: Int = 10
+    println(splitLineAndCountWords(text))
   }
 
 }
