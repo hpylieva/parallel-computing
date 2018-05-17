9836ef5d12d12220c5898b691028a16a6d2c2449
Hanna Pylieva <pylyeva@gmail.com>
2018-05-18 01:08:36 +0300

    WordCount splits only once.

--- src/main/scala/WordCount/WordCount.scala
+++ src/main/scala/WordCount/WordCount.scala
@@ -8,25 +8,84 @@ object WordCount{
   }
 
   object WordCounter{
-//     I need this method to be static (belong to a class not an object of a class)
+    //     I need this method to be static (belong to a class not an object of a class)
     def initWordCounter(text: String): WordCounter = {
-      val words = text.split("\\W+").toList
-//      val firstPart = text.takeWhile(_ != ' ')
-//      val lastPart = text.split(" ").last
-//      val middleWordCount = text.split(" ").length - 2
-//      println(firstPart,middleWordCount, lastPart)
-//      new WordCounter(firstPart, middleWordCount, lastPart)
+      lazy val startsWithSpace = text.startsWith(" ")
+      lazy val endsWithSpace = text.endsWith(" ")
+
+      val words = text.split("\\s+").toList
+
       println(words.head, words.length - 2, words.last)
-      new WordCounter(words.head, words.length - 2, words.last)
+
+      if (startsWithSpace && endsWithSpace) {
+        FullWords(words.head, words.length - 2, words.last)
+      }
+      else if (startsWithSpace){
+        RightUnfinished(words.head, words.length - 2, words.last)
+      }
+      else if (endsWithSpace){
+        LeftUnfinished(words.head, words.length - 2, words.last)
+      }
+      else BothUnfinished(words.head, words.length - 2, words.last)
+
+
+
+//      new FullWords(words.head, words.length - 2, words.last)
     }
   }
 
-  case class WordCounter(leftPart: String, countWords: Int, rightPart: String)
+  sealed trait WordCounter
+  case class FullWords(leftPart: String, countWords: Int, rightPart: String) extends WordCounter
+  case class LeftUnfinished(leftPart: String, countWords: Int, rightPart: String) extends WordCounter
+  case class RightUnfinished(leftPart: String, countWords: Int, rightPart: String) extends WordCounter
+  case class BothUnfinished(leftPart: String, countWords: Int, rightPart: String) extends WordCounter
+  case class Part(chars: String) extends WordCounter
 
   val wordCountMonoid =  new Monoid[WordCounter] {
-    override def op(x: WordCounter, y: WordCounter): WordCounter =
-      WordCounter(x.leftPart, x.countWords + y.countWords + 1, y.rightPart)
-    override def zero: WordCounter = WordCounter("",0,"")
+    override def op(x: WordCounter, y: WordCounter): WordCounter = (x, y) match {
+      case ( FullWords(xLeftPart, xWordCount, xRightPart),
+      FullWords(yLeftPart, yWordCount, yRightPart)) =>
+        FullWords(xLeftPart, xWordCount + yWordCount + 2, yRightPart)
+
+      case ( RightUnfinished(xLeftPart, xWordCount, xRightPart),
+      LeftUnfinished(yLeftPart, yWordCount, yRightPart)) =>
+        FullWords(xLeftPart, xWordCount + yWordCount + 1, yRightPart)
+
+      case ( BothUnfinished(xLeftPart, xWordCount, xRightPart),
+      BothUnfinished(yLeftPart, yWordCount, yRightPart)) =>
+        FullWords(xLeftPart, xWordCount + yWordCount + 1, yRightPart)
+
+//      case (Part(leftPart), Part(rightPart)) =>
+//        Part(leftPart + rightPart)
+//
+//      case (Part(leftPart), FullWord("", wordCount, rightPart)) =>
+//        FullWord(leftPart, wordCount, rightPart)
+//
+//      case (FullWord(leftPart, wordCount, ""), Part(rightPart)) =>
+//        FullWord(leftPart, wordCount, rightPart)
+//
+//      case (Part(NonEmptySpaceString(_)), FullWord(_, wordCount, rightPart)) =>
+//        FullWord("", wordCount + 1, rightPart)
+//
+//      case (FullWord(leftPart, wordCount, _), Part(NonEmptySpaceString(_))) =>
+//        FullWord(leftPart, wordCount + 1, "")
+//
+//      case (Part(leftLeftPart), FullWord(leftRightPart, wordCount, rightPart)) =>
+//        FullWord(leftLeftPart + leftRightPart, wordCount, rightPart)
+//
+//      case (FullWord(leftPart, wordCount, rightLeftPart), Part(rightRightPart)) =>
+//        FullWord(leftPart, wordCount, rightLeftPart + rightRightPart)
+//
+//      case (FullWord(leftPart, leftWordCount, centreLeftPart), FullWord(centreRightPart, rightWordCount, rightPart)) =>
+//        FullWord(
+//          leftPart,
+//          leftWordCount + rightWordCount + (if ((centreLeftPart + centreRightPart) == "") 0 else 1),
+//          rightPart
+//        )
+    }
+
+    //      WordCounter(x.leftPart, x.countWords + y.countWords + 1, y.rightPart)
+    override def zero: WordCounter = FullWords("", 0, "")
   }
 
 
@@ -40,14 +99,18 @@ object WordCount{
       }
     }
 
-    val result = iter(text)
+//    val result = iter(text)
     // add 2 as we have words in left and right parts
-    result.countWords + 2
+    iter(text) match{
+      case FullWords(_, wordsCount, _) =>
+        wordsCount + 2
+    }
+//    result.countWords + 2
   }
 
   def getLinesFromFile(filename: String) = {
-    val lines = Source.fromFile(filename).getLines.mkString("\n")
-//    println(s"The file $filename contains ${lines.length} lines.")
+    val lines = Source.fromFile(filename).getLines.mkString(" ")
+    //    println(s"The file $filename contains ${lines.length} lines.")
     lines
   }
 
@@ -55,7 +118,7 @@ object WordCount{
 
   def main(args: Array[String]): Unit = {
     val text = getLinesFromFile("src/main/scala/WordCount/small.txt")
-    implicit val threshold: Int = 10
+    implicit val threshold: Int = 15
     println(splitLineAndCountWords(text))
   }
 
